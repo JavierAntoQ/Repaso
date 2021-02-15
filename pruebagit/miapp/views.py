625d75aefa0c228eb38e13367d9fa2dc4ccec21a
Javier <1813011329@untels.edu.pe>
2021-02-14 23:51:57 -0500

    styles p1

--- pruebagit/miapp/views.py
+++ pruebagit/miapp/views.py
@@ -2,22 +2,9 @@ from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 layout = """
-    <h1>Proyecto WEB LP3 || Quispe Javier </h1>
-    <hr/>
-        <ul>
-            <li>
-                <a href="/inicio"> INICIO </a> 
-            </li>
-            <li> 
-                <a href="/saludo"> MENSAJE DE SALUDO </a>
-            </li>
-            <li> 
-                <a href="/rango/10/20"> RANGO </a> 
-            </li>
-        </ul>
-    <hr/>
-    """ 
- 
+
+    """
+
 def saludo(request):
     return render(request, 'saludo.html')
 
