1c713231e00c4c1e02aef45cbdfdbaa3e30fb3c5
Javier <1813011329@untels.edu.pe>
2021-02-14 20:43:46 -0500

    primera semana completo

--- pruebagit/miapp/views.py
+++ pruebagit/miapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 layout = """
@@ -12,29 +12,22 @@ layout = """
                 <a href="/saludo"> MENSAJE DE SALUDO </a>
             </li>
             <li> 
-                <a href="/rango/10/20"> RANGO </a>
+                <a href="/rango/10/20"> RANGO </a> 
             </li>
         </ul>
     <hr/>
     """ 
  
 def saludo(request):
-    mensaje = """
-        <h1>BIENVENIDOS A LA UNTELS </h1>
-        <h2>Facultad de Ingenieria </h2>
-        <h3>EP Ingenieria de Sistemas </h3>
-        <h4>LP III - JAVIER QUISPE </h4>
-            """
-    return HttpResponse(layout + mensaje)
+    return render(request, 'saludo.html')
 
 def index(request):
-    mensaje = """
-        <h1>Inicio</h1>
-        """
-    return HttpResponse(layout + mensaje)
+    return render(request, 'index.html')
 
 def rango(request,a,b):
 
+    if a > b:
+        return redirect('rango', a=b, b=a)
     resultado = f"""
         <h1> Rango con parametros </h1>
         <h2> Numero de [{a} , {b}] </h2>
